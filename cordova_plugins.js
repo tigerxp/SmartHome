8e8ccdb2b5352a6885a54f3ee37ca72868486a49
tiger <s.garmash@softerra.com>
2015-01-11 02:56:43 +0200

    Added plugins, changed structure to new format

--- cordova_plugins.js
+++ cordova_plugins.js
@@ -6,12 +6,27 @@ module.exports = [
         "merges": [
             "navigator.notification"
         ]
+    },
+    {
+        "file": "plugins/org.apache.cordova.dialogs/www/notification.js",
+        "id": "org.apache.cordova.dialogs.notification",
+        "merges": [
+            "navigator.notification"
+        ]
+    },
+    {
+        "file": "plugins/org.apache.cordova.dialogs/www/android/notification.js",
+        "id": "org.apache.cordova.dialogs.notification_android",
+        "merges": [
+            "navigator.notification"
+        ]
     }
 ];
 module.exports.metadata = 
 // TOP OF METADATA
 {
-    "org.apache.cordova.vibration": "0.3.8"
+    "org.apache.cordova.vibration": "0.3.8",
+    "org.apache.cordova.dialogs": "0.2.7"
 }
 // BOTTOM OF METADATA
 });
---
+++ scripts/index.js
@@ -0,0 +1,74 @@
+var URL = 'http://tiger.uk.to:8088/api';
+var INTERVAL = 30*1000; // in seconds
+
+var app = {
+
+    snoozedFor: 0,
+
+    initialize: function() {
+        this.bindEvents();
+    },
+
+    bindEvents: function() {
+        document.addEventListener('deviceready', this.onDeviceReady, false);
+    },
+
+    onDeviceReady: function() {
+        app.run.apply(app);
+    },
+
+    run: function() {
+        var self = this;
+        this._checkTemp();
+
+        // document ready
+        $(function(){
+            setInterval(function() {
+                self._checkTemp.apply(self);
+            }, INTERVAL);
+
+            $('#snoozeButton').click(function () {
+                var snooze = parseInt($('#snooze').val());
+                navigator.notification.alert('Snoozed for ' + snooze + ' min',
+                    function () {
+                        self.snoozedFor = snooze*60*1000;
+                    },
+                    'Alert',
+                    'OK'
+                );
+            });
+        });
+
+    },
+
+    _checkTemp: function () {
+        var self = this;
+        $.getJSON(URL, function(data) {
+            if (data && data['temp']) {
+                var temp = data['temp'];
+                var colorClass = data['cls'] ? data['cls'] : '';
+                $('#temp').text(data['temp'].toFixed(2)).attr('class', colorClass);
+                if (self.snoozedFor > 0) {
+                    // Do not check if snoozed
+                    self.snoozedFor -= INTERVAL;
+                } else {
+                    if (colorClass == 'alert') {
+                        self._vibrate();
+                        self._playBeep(3);
+                    }
+                }
+            }
+        });
+    },
+
+    _playBeep: function (count) {
+        navigator.notification.beep(count);
+    },
+
+    _vibrate: function () {
+        navigator.notification.vibrate(300);
+        setTimeout(function() {
+            navigator.notification.vibrate(200);
+        }, 300);
+    }
+};
